22be1e27e16bbc4e4124847501f6f05bdc565d52
Nicolas Guillard <nguillard@et.esiea.fr>
2025-02-26 22:40:30 +0100

    Mise en place du footer.php

---
+++ asset/php/footer.php
@@ -0,0 +1,34 @@
+    <footer class="bg-dark text-light py-4 fixed-bottom w-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mb-3 d-none d-md-block">
+                    <h5 class="fw-bold">Contactez-nous</h5>
+                    <p>Pour toute question ou prise de rendez-vous, contactez-nous :</p>
+                    <ul class="list-unstyled">
+                        <li>Téléphone : 01 23 45 67 89</li>
+                        <li>Email : contact@medecinsite.fr</li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 mb-3 d-none d-md-block text-center">
+                    <h5 class="fw-bold">Notre service</h5>
+                    <p>Gérez vos rendez-vous médicaux facilement et rapidement avec notre plateforme en ligne.</p>
+                </div>
+
+                <div class="col-md-4 mb-3 text-md-end">
+                    <h5 class="fw-bold">Liens utiles</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-light">Mentions légales</a></li>
+                        <li><a href="#" class="text-light">Politique de confidentialité</a></li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="text-center mt-4">
+                <p>&copy; 2025 MesRendezVous - Tous droits réservés</p>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
