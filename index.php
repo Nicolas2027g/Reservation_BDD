a10cc481f413f4fb64a0e08a51255d9679c42344
Nicolas Guillard <nguillard@et.esiea.fr>
2025-02-27 09:11:12 +0100

    Page d'accueil

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php   include "asset/php/head.php"    ?>
 <?php   include "asset/php/header_d.php"  ?>
 
-<main>
-    
+<main class="flex-grow-1 d-flex align-items-center justify-content-center text-center">
+    <div class="container">
+        <h1 class="display-4 mb-4">Bienvenue sur notre plateforme de gestion des rendez-vous médicaux</h1>
+        <p class="lead mb-4">Réservez, gérez et suivez vos rendez-vous médicaux facilement et rapidement, où que vous soyez.</p>
+        <a href="#" class="btn btn-primary btn-lg">Connectez-vous</a>
+    </div>
 </main>
 
 <?php   include "asset/php/footer.php"  ?>
+<?php   include "asset/php/foot.php"    ?>
