ab390be61194836a4210fc6948854e5c944da86f
Nicolas Guillard <nguillard@et.esiea.fr>
2025-02-28 01:44:57 +0100

    Modification du header + calendrier

---
+++ asset/php/header_c.php
@@ -0,0 +1,38 @@
+<header class="bg-dark text-light py-3 w-100">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container">
+            <a class="navbar-brand fw-bold fs-4" href="#">MesRendezVous</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav nav-pills">
+                    <li class="nav-item mx-2">
+                        <a class="nav-link text-light fw-bold px-3 py-2 rounded-pill" href="parametre.php" 
+                           onmouseover="this.classList.add('bg-light', 'text-dark')" 
+                           onmouseout="this.classList.remove('bg-light', 'text-dark')">
+                            Paramètre
+                        </a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link text-light fw-bold px-3 py-2 rounded-pill" href="profil.php" 
+                           onmouseover="this.classList.add('bg-light', 'text-dark')" 
+                           onmouseout="this.classList.remove('bg-light', 'text-dark')">
+                            Mes Rendez Vous
+                        </a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link text-light fw-bold px-3 py-2 rounded-pill" href="profil.php" 
+                           onmouseover="this.classList.add('bg-light', 'text-dark')" 
+                           onmouseout="this.classList.remove('bg-light', 'text-dark')">
+                            Prendre Rendez Vous
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
